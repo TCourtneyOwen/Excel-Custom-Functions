38dc0663e10778fdc48ab94245d178e61a08e047
Courtney Owen <cowen@microsoft.com>
2018-10-30 08:58:54 -0700

    test release pipeline

--- test/customfunctions.js
+++ test/customfunctions.js
@@ -3,7 +3,7 @@ export function add(first, second){
   }
   
 export function increment(incrementBy, callback) {
-    var result = 0;
+    var result = 1000;
     var timer = setInterval(function() {
       result += incrementBy;
       callback.setResult(result);
